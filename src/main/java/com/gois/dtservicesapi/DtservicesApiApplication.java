bafe6ea368c93ac2a36bc044b6af14fab901a4f8
Gois-Prestataire GUSTAVO (KLX) [28212e10-35f0-4a99-af38-4d6fd950fda7] <gustavo.gois-prestataire@ca-ps.com>
2023-01-14 23:43:31 +0000

    Initial commit

---
+++ src/main/java/com/gois/dtservicesapi/DtservicesApiApplication.java
@@ -0,0 +1,13 @@
+package com.gois.dtservicesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtservicesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DtservicesApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gois/dtservicesapi/DtservicesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gois.dtservicesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtservicesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
